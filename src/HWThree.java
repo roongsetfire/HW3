fa5c0813c388ab7b2fffdf8f8a66363471b07395
roong <roongsetfire@gmail.com>
2021-03-23 15:54:24 +0700

    First Project

---
+++ src/HWThree.java
@@ -0,0 +1,29 @@
+
+public class HWThree {
+	public static void main(String[] args) {
+		int i = 20;
+		i++;
+		i++;
+		i++;
+		i++;
+		i++;
+		System.out.println(i);
+		i--;
+		i--;
+		i--;
+		i--;
+		i--;
+		System.out.println(i);
+		
+		float value_1 = 1.5f;
+		float value_2 = 2.5f;
+		if ((value_1 == 1.5) && (value_2 == 2.5))
+		System.out.println("value1 is 1 AND value2 is 2");
+		
+		char c1 = 'A';
+		char c2 = 'B';
+		if ((c1 == 'A') || (c2 == 'C'))
+		System.out.println("c1 or c2");
+	}
+
+}
